3925ae43da896ee0165fc9e9b91c57d3a4e143cd
Laibah Ahmed <108556287+LaibahA@users.noreply.github.com>
2025-01-04 02:26:41 -0500

    Update day2-aoc2025.py

--- day2-aoc2025.py
+++ day2-aoc2025.py
@@ -4,37 +4,20 @@ import csv
 def conditions(levels):
         for i in range(len(levels) - 1):
             if abs(int(levels[i]) - int(levels[i + 1])) < 1 or abs(int(levels[i]) - int(levels[i + 1])) > 3:
-<<<<<<< HEAD
-                    return False
-=======
                 return False
->>>>>>> 0225e51a559a457f932a518201327e88a2735984
         return True
 #This makes sure that its only going up or down, not both
 def trend(levels):
     increase = True
     decrease = True
-<<<<<<< HEAD
-=======
-
->>>>>>> 0225e51a559a457f932a518201327e88a2735984
     for i in range(len(levels) - 1):
         if int(levels[i]) <  int(levels[i+1]):
             decrease = False #its increasing
         elif int(levels[i]) >  int(levels[i+1]):
             increase = False #its decreasing
-<<<<<<< HEAD
     return increase or decrease
 
-#Read each line into a list, checking elements of the list as we go
 safeFloors = 0
-=======
-    
-    return increase or decrease
-
-#Read each line into a list, checking elements of the list as we go
-count = 0
->>>>>>> 0225e51a559a457f932a518201327e88a2735984
 with open('aoc2025-d2.txt', 'r') as file:
     for line in file:
         levels = line.split()
@@ -42,7 +25,6 @@ with open('aoc2025-d2.txt', 'r') as file:
         t1 = conditions(levels)
         t2 = trend(levels)
         if t1 and t2:
-<<<<<<< HEAD
             safeFloors += 1
         else:
             safe = False
@@ -53,7 +35,3 @@ with open('aoc2025-d2.txt', 'r') as file:
             if safe == True:
                     safeFloors += 1
 print(safeFloors)
-=======
-            count += 1
-print(count)
->>>>>>> 0225e51a559a457f932a518201327e88a2735984
