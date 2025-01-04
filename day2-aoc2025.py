241a775f9f08e51a72d01c43b653978ca7c7c10c
LaibahA <laibahinfo05@gmail.com>
2025-01-04 02:16:51 -0500

    finished second part of day 2

--- day2-aoc2025.py
+++ day2-aoc2025.py
@@ -4,23 +4,21 @@ import csv
 def conditions(levels):
         for i in range(len(levels) - 1):
             if abs(int(levels[i]) - int(levels[i + 1])) < 1 or abs(int(levels[i]) - int(levels[i + 1])) > 3:
-                return False
+                    return False
         return True
 #This makes sure that its only going up or down, not both
 def trend(levels):
     increase = True
     decrease = True
-
     for i in range(len(levels) - 1):
         if int(levels[i]) <  int(levels[i+1]):
             decrease = False #its increasing
         elif int(levels[i]) >  int(levels[i+1]):
             increase = False #its decreasing
-    
     return increase or decrease
 
 #Read each line into a list, checking elements of the list as we go
-count = 0
+safeFloors = 0
 with open('aoc2025-d2.txt', 'r') as file:
     for line in file:
         levels = line.split()
@@ -28,5 +26,13 @@ with open('aoc2025-d2.txt', 'r') as file:
         t1 = conditions(levels)
         t2 = trend(levels)
         if t1 and t2:
-            count += 1
-print(count)
+            safeFloors += 1
+        else:
+            safe = False
+            for i in range(len(levels)):
+                temp_levels = levels[:i] + levels[i+1:]
+                if conditions(temp_levels) and trend(temp_levels):
+                        safe = True
+            if safe == True:
+                    safeFloors += 1
+print(safeFloors)
